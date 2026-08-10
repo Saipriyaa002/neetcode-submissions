cd3b0ab346a66a823051ae69d73e9e758cc0ffdb
Sai Priyaa M <saipriyaa.002@gmail.com>
2026-08-10 19:06:34 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        n=[0]*26
+        w=[0]*26
+        for c in s1:
+            n[ord(c)-ord('a')]+=1
+        for i in range(len(s1)):
+            w[ord(s2[i])-ord('a')]+=1
+        if n==w:
+            return True
+        for i in range(len(s1),len(s2)):
+            w[ord(s2[i])-ord('a')]+=1
+            w[ord(s2[i-len(s1)])-ord('a')]-=1
+            if n==w:
+                return True
+        return False
+        
+        '''a=[]
+        def permute(s,l,r):
+            if l==r:
+                a.append("".join(s))
+                return
+            for i in range(l,r+1):
+                s[i],s[l]=s[l],s[i]
+                permute(s,l+1,r)
+                s[i],s[l]=s[l],s[i]
+        l=0
+        r=len(s1)-1
+        b=[]
+        for ch in s1:
+            b.append(ch)
+        permute(b,l,r)
+        for i in a:
+            if i in s2:
+                return True
+        return False'''
